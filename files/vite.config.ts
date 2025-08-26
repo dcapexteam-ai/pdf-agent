40cd901ea41820eff4437ef3c7b1c60d00198e7d
dcapexteam-ai <dcapexteam@gmail.com>
2025-08-26 11:50:31 +0400

    Add files via upload

---
+++ files/vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  optimizeDeps: {
+    include: ['pdfjs-dist/build/pdf', 'pdfjs-dist/build/pdf.worker.min.js']
+  },
+  define: {
+    'process.env': {}
+  }
+})
